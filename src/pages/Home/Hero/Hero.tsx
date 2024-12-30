0e951363e23e0c3e8c283492afd016b58661a7b1
Angelo <angelobispofilho@gmail.com>
2024-12-30 02:12:11 -0300

    feat: adiçao de estilizacao e responsividade

--- src/pages/Home/Hero/Hero.tsx
+++ src/pages/Home/Hero/Hero.tsx
@@ -1,29 +1,51 @@
-import {styled} from "@mui/material";
+import {Button, Container, Grid, styled, Typography} from "@mui/material";
 import React from "react";
-
 import Avatar from "../../../assets/images/perfil.jpg";
 
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import EmailIcon from '@mui/icons-material/Email';
 
 
 const Hero = () => {
 
   const StyledImg = styled('img')(() => ({
-    width: "30%",
+    width: "100%",
     borderRadius: "50%"
   }));
 
 
   const StyledHero = styled('div')(() => ({
     backgroundColor: 'black',
+    height: "100vh"
   }));
 
     return (
       <>
-      
+           <StyledHero>
+          <Container maxWidth="lg">
+          <Grid container spacing={2}>
+      <Grid item xs={12} md={4}>
+      <StyledImg src={Avatar} alt="perfil" />
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Typography color="primary" variant="h1" textAlign="center">Angelo Marcos</Typography>
+        <Typography color="primary"  variant="h2" textAlign="center">I´m Software Engineer</Typography>
         
-        <StyledHero>
-        <StyledImg src={Avatar} alt="perfil" />
-          Ola
+        <Grid container display="flex" justifyContent="center">
+            <Grid item xs={12} md={4} display="flex" justifyContent="center">
+            <Button><FileDownloadIcon/>Download CV</Button>
+            </Grid>
+            <Grid item xs={12} md={4} display="flex" justifyContent="center"> 
+            <Button><EmailIcon/>Contact me</Button>
+        </Grid>
+        </Grid>
+        
+        
+      </Grid>
+      
+    </Grid>
+      
+    </Container>
           </StyledHero>
 
       </>
